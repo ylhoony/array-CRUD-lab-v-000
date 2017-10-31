d46fcc86815dd706cc423df67f637a5cd2ffa044
Hoon Lee <yl.hoony@gmail.com>
2017-10-31 04:18:32 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,9 +28,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.last
 end
